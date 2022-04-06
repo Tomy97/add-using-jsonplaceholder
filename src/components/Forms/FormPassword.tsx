a0345cf2aff3300277d9f7d373bb23935f4ad7ea
Tomas <tomas_padulo@div-it.com.ar>
2022-04-05 22:59:45 -0300

    register form y componentes

--- src/components/Forms/FormPassword.tsx
+++ src/components/Forms/FormPassword.tsx
@@ -1,4 +1,4 @@
-import { useField, useFormik } from 'formik'
+import { FormikHelpers, FormikValues, useField, useFormik } from 'formik'
 import React from 'react'
 import { Form } from 'react-bootstrap'
 import * as yup from 'yup'
@@ -6,6 +6,7 @@ import propTypes from 'prop-types'
 
 interface Props {
   password: string
+  label: string
 }
 
 const validationSchema = yup.object({
@@ -15,23 +16,23 @@ const validationSchema = yup.object({
     .required('Password is required')
 })
 
-export const FormPassword = ({ password }: Props) => {
+export const FormPassword = ({ password, label }: Props) => {
   const { handleChange, values } = useFormik({
     initialValues: {
       password
     },
     validationSchema,
-    onSubmit: (values: any) => {
-      console.log(values.password)
-    }    
+    onSubmit: (values: { password: string }) => {
+      console.log(password);
+    }
   })
 
   return (
     <Form.Group className="mb-3">
-      <Form.Label>Password</Form.Label>
+      <Form.Label>{label}</Form.Label>
       <Form.Control
         type="password"
-        name={'password'}
+        name={password}
         onChange={handleChange}
         placeholder="Enter password"
       />
@@ -41,4 +42,5 @@ export const FormPassword = ({ password }: Props) => {
 
 FormPassword.propTypes = {
   password: propTypes.string.isRequired,
+  label: propTypes.string.isRequired
 }
--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,7 +1,45 @@
+import { FormikValues, useFormik } from 'formik'
 import React from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
+import { BtnSubmit } from '../components/Buttons/BtnSubmit'
+import FormEmail from '../components/Forms/FormEmail'
+import { FormPassword } from '../components/Forms/FormPassword'
 
 export const Register = () => {
+  const { handleChange, values, handleSubmit } = useFormik<FormikValues>({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: (values: FormikValues) => {
+      console.log(values.email, values.password)
+    }
+  })
   return (
-    <div>Register</div>
+    <Row>
+      <Col
+        className="d-flex justify-content-center align-content-center "
+        md={12}
+      >
+        <Card style={{ width: '30rem', height: '20rem' }}>
+          <Card.Body>
+            <Card.Title className="text-center">Register Form</Card.Title>
+            <Card.Text>
+              <form onSubmit={handleSubmit}>
+                <Row>
+                  <Col md={12}>
+                    <FormEmail />
+                  </Col>
+                  <Col md={12}>
+                    <FormPassword password={values.password} label="Password" />
+                  </Col>
+                </Row>
+              </form>
+            </Card.Text>
+            <BtnSubmit />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   )
 }
